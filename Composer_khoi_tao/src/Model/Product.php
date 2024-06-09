769b3676db1b46c2c81def61628a4c5148b96630
phanquoc123 <phanquoc1512@gmail.com>
2024-06-09 22:56:23 +0700

    comott

--- Composer_khoi_tao/src/Model/Product.php
+++ Composer_khoi_tao/src/Model/Product.php
@@ -120,23 +120,7 @@ class Product extends Model
 
         return [$data, $totalPage];
     }
-<<<<<<< HEAD
-    // public function paginateProducts($page = 1, $perPage = 5)
-    // {
-    //     $queryBuilder = clone ($this->queryBuilder);
-    //     $totalPage = ceil($this->count() / $perPage);
-    //     $offset = $perPage * ($page - 1);
-    //     $data = $queryBuilder
-    //         ->select('*')
-    //         ->from($this->tableName)
-    //         ->setFirstResult($offset)
-    //         ->setMaxResults($perPage)
-    //         ->fetchAllAssociative();
 
-    //     return [$data, $totalPage];
-    // }
-=======
->>>>>>> 305d7f756da4a215a5573573f3a4ccf9a75ab1aa
     public function findByID($id)
     {
         return $this->queryBuilder
